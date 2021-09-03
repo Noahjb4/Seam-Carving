60871e4e656814c4c01c741b8d1a33a3b016534c
Noahjb4 <noahreed1@gmail.com>
2021-09-03 13:53:40 -0400

    User Input / Image Test

--- sc.py
+++ sc.py
@@ -1,2 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os, sys
+from imageio import imread, imwrite
+
+
+def main():
+
+    #img = imread(sys.argv[1])
+    img = imread('dice.png')
+    #print(img)
+
+    imwrite('dice2.png', img[:len(img)//2])
+
+if __name__ == '__main__':
+    main()
